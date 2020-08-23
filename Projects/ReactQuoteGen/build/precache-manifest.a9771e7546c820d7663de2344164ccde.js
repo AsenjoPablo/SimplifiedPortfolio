9586d0944948c5e60c678a5acd7fe50d63abfeb3
Pablo Asenjo <asenjo.serrano@gmail.com>
2020-08-23 13:13:54 +0200

    CodeGenBuild

---
+++ Projects/ReactQuoteGen/build/precache-manifest.a9771e7546c820d7663de2344164ccde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12c3618d89a8bd43b6d9d1697830bf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69714344eb53ec416bda",
+    "url": "/static/css/main.e3348f9d.chunk.css"
+  },
+  {
+    "revision": "9fea0519979ef7dad1ae",
+    "url": "/static/js/2.d70cad23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d70cad23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69714344eb53ec416bda",
+    "url": "/static/js/main.c712d043.chunk.js"
+  },
+  {
+    "revision": "da09c51b7cde2e2711ac",
+    "url": "/static/js/runtime-main.80b58a73.js"
+  }
+]);
